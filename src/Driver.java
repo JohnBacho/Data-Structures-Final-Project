6aff03af8a594433ed78f35d4a7a8198740d79ee
JohnBacho <jbacho22@bw.edu>
2023-10-25 21:26:32 -0400

    add numbers 1-1000 to the dictionary

--- src/Driver.java
+++ src/Driver.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class Driver {
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Enter a 1 to start program. Enter a 2 to add to dictionary");
+            System.out.println("Enter a 1 to start program. Enter a 2 to add a word to the dictionary");
             Scanner scan = new Scanner(System.in);
             int number = scan.nextInt();
-            if (number != 1 || number != 2) {
+            if (number != 1 && number != 2) {
                 System.out.println("invaild input");
-            }
-            if (number == 1) {
+            } else if (number == 1) {
                 LevenshteinDistance.start();
-            }
-            if (number == 2) {
+            } else if (number == 2) {
                 System.out.println("Enter the word you'd like to add");
                 Scanner look = new Scanner(System.in);
                 String letter = look.nextLine();
