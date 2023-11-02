72c5b3a096973983b1b9d53473ffc89375ba154d
kenmamich <mamichkj@gmail.com>
2023-11-02 12:14:33 -0400

    Binary Search Tree Started

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,4 +1,15 @@
 public class BinarySearchTree {
-    
+    Node root;
+
+    private static void insert(String word, Node root){
+        if( root == null){
+            return;
+        }
+        if (word.compareTo(root.getWord())<0){
+            if(root.getLeftChild() == null){
+                root.setLeftChild(new Node(word));
+            }
+        }
+    }
 
 }
